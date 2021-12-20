6998fc1cfc467f61bfd97ca0111d52f5a7261207
mjovenil <86357807+mjovenil@users.noreply.github.com>
2021-12-20 17:28:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,18 @@ import setuptools
 from setuptools import setup, find_packages
 import pip
 
+with pathlib.Path('requirements.txt').open() as requirements_txt:
+    install_requires = [
+        str(requirement)
+        for requirement
+        in pkg_resources.parse_requirements(requirements_txt)
+    ]
+    
 setup(
-    name='sonarcode',
+    name="sonarcode",
     packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
+    version="0.1.0",
+    description="A short description of the project.",
+    install_requires=install_requires,
     author='jovenil',
 )
