0ae11f37beee256a4081a7ffb7e2ffbefbb32e51
mjovenil <86357807+mjovenil@users.noreply.github.com>
2022-01-13 08:44:21 -0300

    Update treinamento.py

--- sonarcode/treinamento.py
+++ sonarcode/treinamento.py
@@ -20,3 +20,18 @@ def format_inputlstm(data):
   data_array = np.array(data[:])
   x_out = data_array.reshape(data_array.shape[0],data_array.shape[1],1)
   return x_out
+
+
+# Difinindo quem é o melhor modelo
+def melhor_modelo(sp_fold,val_fold):  
+  n_max = sp_fold.count(max(sp_fold))
+  if n_max > 1:
+    ind_model = [(n) for n, x in enumerate(sp_fold) if x== max(sp_fold)] # Quais indices?
+    val_model = [val_fold[x] for x in ind_model] # Valores de validação dos indices
+    best_model = val_model.index(max(val_model))
+  else:
+    best_model = sp_fold.index(max(sp_fold))
+    return best_model
+  
+  
+  
