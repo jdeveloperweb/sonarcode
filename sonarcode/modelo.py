1f6fa0d4b0ae282a572edec020ef2d9d191473c3
mjovenil <86357807+mjovenil@users.noreply.github.com>
2022-01-16 00:12:14 -0300

    Update modelo.py

--- sonarcode/modelo.py
+++ sonarcode/modelo.py
@@ -32,17 +32,23 @@ def modelomlp(data, num_classes, model_config):
 
 def modelocnn(data, num_classes, model_config):
   model = Sequential()
-  input_shape=(data.shape[1], 1)
-  model.add(Conv1D(32, kernel_size=5,padding = 'same', activation='relu', input_shape=input_shape))
-  model.add(LeakyReLU(alpha=0.2))
-  model.add(MaxPooling1D())
-  model.add(Dropout(0.5))
-  model.add(Conv1D(32, kernel_size=5,padding = 'same',activation='relu'))
-  model.add(LeakyReLU(alpha=0.2))
-  model.add(MaxPooling1D())
-  model.add(Dropout(0.5))
+  input_shape=(data.shape[1:])
+  model.add(Conv2D(model_config.neucnn_1, kernel_size=model_config.kernel_size,padding = 'same', activation=model_config.funcactiv, input_shape=input_shape))
+  if model_config.use_drop:
+    model.add(Dropout(model_config.drop))
+  if model_config.neucnn_2 != 0:
+    model.add(Conv2D(model_config.neucnn_2, kernel_size=model_config.kernel_size,padding = 'same',activation=model_config.funcactiv))
+    if model_config.use_drop:
+      model.add(Dropout(model_config.drop))
   model.add(Flatten())
-  model.add(Dense(50, activation='relu'))
-  model.add(Dropout(0.5))
-  model.add(Dense(num_classes, activation='softmax'))
+  if model_config.neumlp_1 != 0:
+    model.add(Dense(model_config.neumlp_1, activation=model_config.funcactiv))
+    if model_config.use_drop:
+      model.add(Dropout(model_config.drop))  
+  if model_config.neumlp_2 != 0:
+    model.add(Dense(model_config.neumlp_2, activation=model_config.funcactiv))
+    if model_config.use_drop:
+      model.add(Dropout(model_config.drop))
+  model.add(Dense(num_classes, activation=model_config.funcout))
+  model.compile(optimizer=model_config.optimizer, loss=model_config.loss, metrics=[model_config.metrics])
   return model
