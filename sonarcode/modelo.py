36b38ecde83c3614ee87c81452b178beb3604dab
mjovenil <86357807+mjovenil@users.noreply.github.com>
2022-02-14 15:22:28 -0300

    Update modelo.py

--- sonarcode/modelo.py
+++ sonarcode/modelo.py
@@ -16,6 +16,8 @@ def modelolstm(n_timesteps, n_features, n_outputs, model_config):
   # if model_config.neumlp_1.isalnum():
   if model_config.neumlp_1 != 0:
     model.add(Dense(model_config.neumlp_1, activation=model_config.funcactiv))
+    if model_config.use_drop:
+      model.add(Dropout(model_config.drop))
   model.add(Dense(n_outputs, activation=model_config.funcout))
   model.compile(optimizer= model_config.optimizer, loss= model_config.loss, metrics=[model_config.metrics])
   return model
